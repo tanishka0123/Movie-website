d19e2ce3f04b7b1a4703a4734b3f9296ac3dccf8
tanishka0123 <guptatanishka2301@gmail.com>
2025-07-05 16:05:11 +0530

    backend

--- server/models/User.js
+++ server/models/User.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  _id: { type: String, required: true, unique: true },
+  _id: { type: String, required: true },
   name: { type: String, required: true },
   email: { type: String, required: true },
   image: { type: String, required: true },
