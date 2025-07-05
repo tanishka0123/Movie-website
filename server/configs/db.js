5720d2375ddbb339ea4620e0cd1ee3392deee95f
tanishka0123 <guptatanishka2301@gmail.com>
2025-07-05 14:34:31 +0530

    backend

---
+++ server/configs/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    mongoose.connection.on("connected", () =>
+      console.log("Database connected successfully")
+    );
+    await mongoose.connect(`${process.env.MONGODB_URI}/movie`);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export default connectDB;
